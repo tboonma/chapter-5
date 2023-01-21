19cb3a526df5cfb47c0d59bca10b3b00d2b7d914
tboonma <tboonma@hotmail.com>
2023-01-21 21:41:16 +0700

    Finished assignment

--- example-4/recommendations/src/index.js
+++ example-4/recommendations/src/index.js
@@ -59,6 +59,7 @@ function setupHandlers(app, db, messageChannel) {
         console.log(JSON.stringify(parsedMsg, null, 4)); // JUST PRINTING THE RECEIVED MESSAGE.
 
         // ... ADD YOUR CODE HERE TO PROCESS THE MESSAGE ...
+        console.log("recommendation video " + parsedMsg.videoId)
 
         console.log("Acknowledging message was handled.");
 
